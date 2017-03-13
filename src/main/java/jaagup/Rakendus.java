765f317f4fc57692519cab86f27dece8882182bb
stanmaje <stanmaje@greeny.cs.tlu.ee>
2017-03-13 13:05:48 +0200

    test

---
+++ src/main/java/jaagup/Rakendus.java
@@ -0,0 +1,23 @@
+package jaagup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping; 
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@SpringBootApplication
+public class Rakendus {
+	@RequestMapping("/algus")
+    String tervitusfunktsioon() {
+        return "Ahoi!";
+    }
+ 
+    public static void main(String[] args) {
+		//System.getProperties().put("server.port", 4201);
+        SpringApplication.run(Rakendus.class, args);
+    }
+}
+
+//scl enable rh-maven33 bash
+//mvn package
+//java -jar target/boot3-1.0-SNAPSHOT.jar
